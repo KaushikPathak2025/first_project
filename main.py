7d5a0b86b8b59652daf9aff308a938fa6940f310
KaushikPathak2025 <kpathakbhs1980@gmail.com>
2025-05-01 13:20:34 +0530

    Third updat

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Hello")
 print("World")
+
+
+# git init [self new start]/ git clone "repo https/ssh link"
+# git status
+# git add
+# git commit -m "Changes"
+# git push
+# gh login [Github login from vs code powershell, cmd, wsl] 
+# git pull
+# git branch
